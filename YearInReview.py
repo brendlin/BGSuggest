5bed4cafa7546e5437f94487f322f9f0bba4b764
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2017-10-22 22:17:06 +0200

    Updating YIR plot

--- YearInReview.py
+++ YearInReview.py
@@ -1,5 +1,5 @@
 from PyBGSuggestHelpers import a1cToBS,RootMeanSquare,TimeClass
-import pennSoftLepton.PlotFunctions as plotfunc
+import PlotFunctions as plotfunc
 import ROOT
 from array import array
 
@@ -8,7 +8,11 @@ def GetAverageBGs(hist,tree,last_week,n_weeks) :
         req = 'BGReading > 00 && WeekOfYear <= %d && WeekOfYear > %d-%d'%(i+1,i+1,n_weeks)
         #req = 'BGReading > 00 && WeekOfYear == %d'%(i)
         n = tree.Draw('BGReading>>hist%d(100,0,500)'%(i+1),req,'goff')
-        if not n : continue
+        if not n :
+            hist.SetBinContent(i+1,hist.GetBinContent(i))
+            hist.SetBinError(i+1,hist.GetBinError(i))
+            continue
+
         Avg = 0
         vals = []
         for x in range(n) :
@@ -16,14 +20,18 @@ def GetAverageBGs(hist,tree,last_week,n_weeks) :
             vals.append(tree.GetV1()[x])
         Avg = Avg / float(n)
 
-        if Avg == 0 : continue
+        if Avg == 0 :
+            hist.SetBinContent(i+1,hist.GetBinContent(i))
+            hist.SetBinError(i+1,hist.GetBinError(i))
+            continue
+
         hist.SetBinContent(i+1,Avg)
         hist.SetBinError(i+1,RootMeanSquare(vals))
 
     return
 
 #-------------------------------------------------------------------------
-def YearInReview(tree,nyears=4) :
+def YearInReview(tree,nyears=5) :
 
     #
     # Requred setup
@@ -63,29 +71,13 @@ def YearInReview(tree,nyears=4) :
 
     a1x = []
     a1y = []
-    # May 30, 2012 : 7.7 (last result from SHS)
-    # january
-    a1x.append(t.GetWeekOfYear(t.TimeFromString('01/12/13 04:00:00')))
-    a1y.append(a1cToBS(7.9)[0])
-    # april
-    a1x.append(t.GetWeekOfYear(t.TimeFromString('04/13/13 04:00:00')))
-    a1y.append(a1cToBS(8.0)[0])
-    # august
-    a1x.append(t.GetWeekOfYear(t.TimeFromString('08/19/13 04:00:00')))
-    a1y.append(a1cToBS(7.7)[0])
-    # december
-    a1x.append(t.GetWeekOfYear(t.TimeFromString('12/19/13 04:00:00')))
-    a1y.append(a1cToBS(7.6)[0])
-    # april
-    a1x.append(t.GetWeekOfYear(t.TimeFromString('04/08/14 04:00:00')))
-    a1y.append(a1cToBS(7.3)[0])
-    # december 2014
-    a1x.append(t.GetWeekOfYear(t.TimeFromString('12/16/14 04:00:00')))
-    a1y.append(a1cToBS(7.4)[0])
-    # september 2015
-    a1x.append(t.GetWeekOfYear(t.TimeFromString('09/02/15 04:00:00')))
-    a1y.append(a1cToBS(7.6)[0])
-    #
+    for line in open('data/a1c.csv').readlines() :
+        line = line.replace('\n','')
+        date = line.split(',')[0].lstrip(' ').rstrip(' ')
+        a1c  = line.split(',')[1].lstrip(' ').rstrip(' ')
+        a1x.append(t.GetWeekOfYear(t.TimeFromString('%s 04:00:00'%(date))))
+        a1y.append(a1cToBS(float(a1c))[0])
+
     x_a1 = array('d',a1x)
     y_a1 = array('d',a1y)
     a1cs = ROOT.TGraph(len(x_a1),x_a1,y_a1)
@@ -96,23 +88,9 @@ def YearInReview(tree,nyears=4) :
     a1cs.SetMarkerColor(a1c_color)
     a1cs.SetMarkerStyle(20)
     a1cs.SetMarkerSize(1.5)
-    a1cs.SetLineWidth(2)
+    a1cs.SetLineWidth(3)
     a1cs.SetLineColor(a1c_color)
     a1cs.SetMarkerStyle(22)
-
-#     dayspermonth = {0 :['Jan', 31],
-#                     1 :['Feb', 28],
-#                     2 :['Mar', 31],
-#                     3 :['Apr', 30],
-#                     4 :['May', 31],
-#                     5 :['Jun', 30],
-#                     6 :['Jul', 31],
-#                     7 :['Aug', 31],
-#                     8 :['Sep', 30],
-#                     9 :['Oct', 31],
-#                     10:['Nov', 30],
-#                     11:['Dec', 31]
-#                     }
 
     labels = {0:'Jan-Mar',
               1:'Apr-Jun',
@@ -120,7 +98,7 @@ def YearInReview(tree,nyears=4) :
               3:'Oct-Dec'
               }
 
-    dummy = ROOT.TH1F('dummy','dummy',nyears*4,0,365*nyears/7)
+    dummy = ROOT.TH1F('dummy','remove',nyears*4,0,365*nyears/7)
     for mo3 in range(nyears * 4 - 1) :
         dummy.GetXaxis().SetBinLabel(mo3+1,'%s \'%d     '%(labels[mo3%4],13+mo3/4))
 
@@ -137,8 +115,6 @@ def YearInReview(tree,nyears=4) :
     year_in_review.SetLeftMargin(0.16/2.)
     year_in_review.SetRightMargin(0.16/2.)
     year_in_review.SetBottomMargin(0.12)
-    
-    yir_17w.SetFillStyle(1001)
 
     def Prepare(hist,color) :
         hist.SetLineColor(color)
@@ -146,25 +122,26 @@ def YearInReview(tree,nyears=4) :
         if hist.GetMarkerColor() == 0 :
             hist.SetMarkerColor(1)
         hist.SetFillColor(color)
-        hist.SetLineWidth(2)
+        hist.SetLineWidth(3)
         return
 
     Prepare(yir_17w,ROOT.kAzure-2)
     Prepare(yir,ROOT.kBlack)
     Prepare(yir_smooth,ROOT.kRed+1)
 
-#     yir_17w.SetFillColor(ROOT.kBlue-10)
-#     yir_smooth.SetFillColor(ROOT.kRed-10)
-    yir_17w.SetFillStyle(3154)
-    yir_smooth.SetFillStyle(3154)
+    yir_17w.SetFillStyle(3001)
+    yir_smooth.SetFillStyle(0)
 
     plotfunc.AddHistogram(year_in_review,dummy)
-    yir_17w.SetTitle('skipme')
-    plotfunc.AddHistogram(year_in_review,yir_17w,'E3')
-    yir_17w.SetTitle('17-week average')
+    yir_17w.SetMarkerSize(0)
+    yir_17w.SetTitle('remove')
+    plotfunc.AddHistogram(year_in_review,yir_17w,'lE3')
     plotfunc.AddHistogram(year_in_review,yir,'phist')
-    plotfunc.AddHistogram(year_in_review,yir_smooth,'phist')
-    plotfunc.AddHistogram(year_in_review,yir_17w,'phist')
+    plotfunc.AddHistogram(year_in_review,yir_smooth,'lhist')
+    # Add yir on top once more.
+    yir_17w.SetFillStyle(0)
+    yir_17w.SetTitle('17-week average')
+    plotfunc.AddHistogram(year_in_review,yir_17w,'lhist')
     plotfunc.AddHistogram(year_in_review,a1cs,'pl')
     plotfunc.AutoFixAxes(year_in_review)
     plotfunc.SetAxisLabels(year_in_review,'','<BG> (mg/dL)')
@@ -186,8 +163,8 @@ def YearInReview(tree,nyears=4) :
     a.DrawLine(0,high,nweeks,high)
     a.DrawLine(last_week-16,low,last_week-16,high)
 
-    option = ['p','p','fp','pl']
-    plotfunc.MakeLegend(year_in_review,0.7,0.15,0.93,0.35,skip=['dummy'],option=option)
+    option = ['p','l','l','pl','pl']
+    plotfunc.MakeLegend(year_in_review,0.7,0.15,0.86,0.35,option=option)
     year_in_review.GetPrimitive('legend').SetFillColor(0)
     year_in_review.GetPrimitive('legend').SetFillStyle(1001)
 
