7c6463e1ff0d051f26686c5597fb9da788f54b89
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2017-10-23 14:04:30 +0200

    Removing Quick macro

--- Quick.py
+++
@@ -1,9 +0,0 @@
-from PlotMacro import WeekPlot,t,BGFunction
-from ROOT import TCanvas
-c = WeekPlot()
-c.Detailed('last')
-c.Overview('last')
-c.YearInReview()
-#c.DoSample()
-#c.SuppliesSummary()
-
